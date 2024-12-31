6e6cf9e58a3e192524ab92eb8ff7a95ef9b766cf
Toby Cm <toby@vsus.app>
2024-12-30 20:21:07 -0800

    init xd

---
+++ README.md
@@ -0,0 +1,23 @@
+# image-sizer - A simple tool to resize and convert your images for the web
+
+## Usage:
+
+### Install dependencies
+
+```bash
+bun install
+```
+
+```bash
+bun run index.ts [-f/--formats formats] [-s/--sizes sizes] [-o/--output output_folder] [-F/--folderforeach] image_file
+```
+
+`--formats` (`-f`): comma-seperated list of formats to convert the images to (default: webp,png)
+
+`--sizes` (`-s`): comma-seperated list of width sizes to resize the images to (default: 16,32,64,128,192,256,384,512,768,1024)
+
+`--output` (`-o`): path to output folder. will be created if it doesn't exist (default: dist/)
+
+`--folderforeach` (`-F`): a toggle flag to organize each generated pictures into their own folder (default: true)
+
+This project was created using `bun init` in bun v1.1.42. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ index.ts
@@ -0,0 +1,115 @@
+import { existsSync, mkdirSync } from "fs";
+import Magic from "magickwand.js";
+import path from "path";
+import { parseArgs } from "util";
+const { Magick } = await Magic;
+
+const { values: args, positionals } = parseArgs({
+  args: process.argv,
+  options: {
+    formats: {
+      type: "string",
+      short: "f",
+      default: "webp,png",
+      description: "Comma-separated list of formats to generate",
+    },
+    sizes: {
+      type: "string",
+      short: "s",
+      default: "16,32,64,128,192,256,384,512,768,1024",
+      description: "Comma-separated list of sizes to generate",
+    },
+    output: {
+      type: "string",
+      short: "o",
+      default: "dist",
+      description: "Output directory",
+    },
+    folderforeach: {
+      type: "boolean",
+      short: "F",
+      default: true,
+      description: "Create a folder for each image",
+    },
+    srcgen: {
+      type: "string",
+      default: "source",
+      description: "Comma-separated list sources to generate",
+    },
+    convertprogram: {
+      type: "string",
+      default: "convert",
+      description: "Path to the ImageMagick convert program",
+    },
+  },
+  strict: true,
+  allowPositionals: true,
+});
+
+const getOutput = (filePath: string) => path.join(args.output!, args.folderforeach ? filePath.replace(/\.[^.]*$/, "") : "");
+
+const formats = args.formats!.split(",");
+const sizes = args
+  .sizes!.split(",")
+  .map((size) => parseInt(size, 10))
+  .sort((a, b) => b - a); // descending
+const srcgen = args.srcgen!.split(",");
+
+let images = positionals.slice(2).map((image) => {
+  return {
+    path: image,
+  };
+});
+
+const loadedImages = await Promise.all(
+  images.map(async (image) => {
+    const im = new Magick.Image();
+    await im.readAsync(image.path);
+    const size = await im.sizeAsync();
+    return {
+      ...image,
+      loaded: im,
+      size,
+    };
+  })
+);
+
+for (const image of loadedImages) {
+  const output = getOutput(image.path);
+
+  if (!existsSync(output)) mkdirSync(output);
+
+  const mySizes = sizes.filter((size) => size < image.size.width());
+
+  for (const size of mySizes) {
+    await image.loaded.thumbnailAsync(`${size}x0\>`);
+
+    for (const format of formats) {
+      await image.loaded.magickAsync(format);
+
+      const filename = args.folderforeach ? `x${size}.${format}` : image.path.replace(/\.[^.]*$/, `-${size}.${format}`);
+      await image.loaded.writeAsync(path.join(output, filename));
+    }
+  }
+
+  if (srcgen.includes("source")) {
+    const img = `<img src="${image.path}">`;
+
+    const sources = formats.map(
+      (format) =>
+        `<source srcset="${sizes
+          .map(
+            (size) =>
+              `${path.join(output, args.folderforeach ? `x${size}.${format}` : image.path.replace(/\.[^.]*$/, `-${size}.${format}`))} ${size}w`
+          )
+          .join(", ")}" type="image/${format}">`
+    );
+
+    console.log(
+      `<picture>
+  ${sources.join("\n  ")}
+  ${img}
+</picture>`
+    );
+  }
+}
